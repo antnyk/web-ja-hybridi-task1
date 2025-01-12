ade66b6e70339d44e0a10009719addedfac073bd
antnyk <anttipnykanen@gmail.com>
2025-01-12 16:40:11 +0200

    task1 done

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [result, setResult] = useState('')
+ //
+  const calculate = () => {
+    const lowerLimit = (220 - age) * 0.65
+    const upperLimit = (220 - age) * 0.85
+
+    let result1 = `${lowerLimit} - ${upperLimit}`
+
+    setResult(result1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Heart rate calculator</Text>
+      <Text>Age</Text>
+      <TextInput
+        style={styles.field}
+        value={age}
+        onChangeText = {(text) => setAge(text)}
+      />
+      <Text style={styles.text1}>{age}</Text>
+      <Text>Limits</Text>
+      <Text>{result}</Text>
+      <Button title="Calculate" onPress={() => calculate()}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  field: {
+    marginBottom: 10,
+    borderColor: '#20232a',
+    borderWidth: 1,
+    backgroundColor: '#61dafb'
+  },
+  title: {
+    marginBottom: 10,
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
 });
